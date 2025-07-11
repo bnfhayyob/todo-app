37a0570f3e08cdb312399fb111a260fc14525ca4
bnfhayyob <boyabenfath@gmail.com>
2025-07-11 05:23:26 +0200

    new mobile app here just making things much easier

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,38 @@
+import { Ionicons } from '@expo/vector-icons'
+import { Tabs } from 'expo-router'
+
+const TabsLayout = () => {
+    return (
+        <Tabs screenOptions={{
+            tabBarActiveTintColor:"red",
+            tabBarInactiveTintColor:"green",
+            tabBarStyle:{
+                backgroundColor:"#1e293b",
+                borderTopWidth:1,
+                borderTopColor:"yellow",
+                height:90,
+                paddingBottom:30,
+                paddingTop:10,
+            }
+        }}>
+            <Tabs.Screen
+                name='index'
+                options={{
+                    title: "Todos",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name='flash-outline' size={size} color={color} />
+                    )
+                }} />
+            <Tabs.Screen
+                name='settings'
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name='settings' size={size} color={color} />
+                    )
+                }} />
+        </Tabs>
+    )
+}
+
+export default TabsLayout
